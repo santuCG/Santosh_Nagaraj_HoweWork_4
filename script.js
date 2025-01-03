05aa9e8a8c7f4665fe4c641fe37fc579e99903e4
santuCG <113492285+santuCG@users.noreply.github.com>
2025-01-03 16:08:52 +0530

    Add files

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('studentForm').addEventListener('submit', function (e) {
+  e.preventDefault();
+  alert('Form submitted successfully! Make sure all inputs are correct.');
+});
